976dd929aa8a176ea40aac3ff5f5893fc2a41fd8
Kotablip <71233489+Kotablip@users.noreply.github.com>
2025-10-03 01:35:58 -0500

    formatted everything with biome

--- src/testJS.jsx
+++ src/testJS.jsx
@@ -3,8 +3,7 @@ import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./routes";
 
 export const jsRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/js",
-  component: () => <div>Hello from a JS route</div>,
+	getParentRoute: () => rootRoute,
+	path: "/js",
+	component: () => <div>Hello from a JS route</div>,
 });
-
