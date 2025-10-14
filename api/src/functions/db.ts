ac832e52726d13ddf320ee395e05b9e814b67bc0
Sushanta Timalsina <sushanttimalsina1@gmail.com>
2025-10-13 21:14:31 -0500

    changes made in api

--- api/src/functions/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const db = new Pool({
-    user: "postgres",           // your PostgreSQL username
-    host: "127.0.0.1",
-    database: "postgres",   // your database name
-    password: "postgres",   // your PostgreSQL password
-    port: 5432,
-});
