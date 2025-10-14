2dfcf524d6da826238e737d574d683a62f11d370
Sushanta Timalsina <sushanttimalsina1@gmail.com>
2025-10-13 23:19:06 -0500

    fixed tsconfig issue

--- api/src/functions/userRegister.ts
+++ api/src/functions/userRegister.ts
@@ -1,43 +1,7 @@
-/*import {
-    app,
-    type HttpRequest,
-    type HttpResponseInit,
-    type InvocationContext,
-} from "@azure/functions";
-import bcrypt = require("bcryptjs")
 
-interface RegisterRequestBody {
-    username: string;
-    password: string;
-}
-
-const users: Record<string, { password: string }> = {};
-
-export async function register(
-    request: HttpRequest,
-    _context: InvocationContext, 
-): Promise<HttpResponseInit> {
-    const { username, password } = await request.json() as RegisterRequestBody;
-
-    if (!username || !password) {
-        return { status: 400, body: "Username and password required" };
-    }
-    if (users[username]) {
-        return { status: 409, body: "Username already exists" };
-    }
-    const hashed = await bcrypt.hash(password, 10);
-    users[username] = { password: hashed };
-    return { status: 201, body: "User registered successfully" };
-}
-
-app.http("register", {
-    methods: ["POST"],
-    authLevel: "anonymous",
-    handler: register,
-});*/
 import { app, type HttpRequest, type HttpResponseInit, type InvocationContext } from "@azure/functions";
 import bcrypt from "bcryptjs";
-import  {db}  from "../functions/db";
+
 
 interface RegisterRequestBody {
     username: string;
