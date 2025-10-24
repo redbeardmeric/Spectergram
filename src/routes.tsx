8ccdad63e5ee61bf55cccff42cb4d520551ffbb2
Kotablip <71233489+Kotablip@users.noreply.github.com>
2025-10-23 19:53:34 -0500

    Ran format again?

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,51 +9,51 @@ import LoginUI from "./ui/LoginUI.tsx";
 import SignupUI from "./ui/SignupUI.tsx";
 
 export const rootRoute = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+	component: () => (
+		<>
+			<Outlet />
+			<TanStackRouterDevtools />
+		</>
+	),
 });
 
 // main (home) route
 export const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/",
-  component: App,
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: App,
 });
 
 // login route
 export const loginRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/login",
-  component: LoginUI,
+	getParentRoute: () => rootRoute,
+	path: "/login",
+	component: LoginUI,
 });
 
 // signup route
 export const signupRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/signup",
-  component: SignupUI,
+	getParentRoute: () => rootRoute,
+	path: "/signup",
+	component: SignupUI,
 });
 
 // friend list route
 export const friendsRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/friends",
-  component: FriendListUI,
+	getParentRoute: () => rootRoute,
+	path: "/friends",
+	component: FriendListUI,
 });
 
 // chat route
 export const chatRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/chat",
-  component: ChatUI,
+	getParentRoute: () => rootRoute,
+	path: "/chat",
+	component: ChatUI,
 });
 
 export const dashboardRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/chat",
-  component: ChatDashboard,
+	getParentRoute: () => rootRoute,
+	path: "/chat",
+	component: ChatDashboard,
 });
